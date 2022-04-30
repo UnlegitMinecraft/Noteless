0b097cbc9cd82851c9f50b0786e20d25aab4a2fe
Insane <3322545884@qq.com>
2022-04-30 17:19:07 +0800

    Criticals

--- 1.8.9-Forge/src/main/java/net/ccbluex/liquidbounce/features/module/modules/combat/Criticals.kt
+++ 1.8.9-Forge/src/main/java/net/ccbluex/liquidbounce/features/module/modules/combat/Criticals.kt
@@ -27,6 +27,7 @@ class Criticals : Module() {
         arrayOf(
             "Packet",
             "NCPPacket",
+            "NewPacket",
             "NoGround",
             "Redesky",
             "AACv4",
@@ -88,7 +89,12 @@ class Criticals : Module() {
                     sendCriticalPacket(yOffset = 1.1E-5, ground = false)
                     sendCriticalPacket(ground = false)
                 }
-
+                "newpacket" -> {
+                    mc.netHandler.addToSendQueue(C04PacketPlayerPosition(x, y + 0.05250000001304, z, true))
+                    mc.netHandler.addToSendQueue(C04PacketPlayerPosition(x, y + 0.00150000001304, z, false))
+                    mc.netHandler.addToSendQueue(C04PacketPlayerPosition(x, y + 0.01400000001304, z, false))
+                    mc.netHandler.addToSendQueue(C04PacketPlayerPosition(x, y + 0.00150000001304, z, false))
+                }
                 "ncppacket" -> {
                     sendCriticalPacket(yOffset = 0.11, ground = false)
                     sendCriticalPacket(yOffset = 0.1100013579, ground = false)
